9f4779e180d34c4ec3b28ffbbe65772970bf37ee
Brandon Tsang <brandononline2@gmail.com>
2018-06-16 15:22:36 -0400

    added video

---
+++ index.js
@@ -0,0 +1,21 @@
+let videoFrame = 0;
+let pixelsPerFrame = 10;
+let pageHeight, video, loop;
+
+$(function() {
+    video = document.getElementById('video');
+
+    video.addEventListener('canplay', function() {
+        pageHeight = video.duration * 24 * pixelsPerFrame;
+        document.getElementById('set-height').style.height = pageHeight + 'px';
+
+        loop = true;
+    });
+
+    setInterval(function() {
+        if (loop) {
+            videoFrame = (window.pageYOffset / pageHeight) * video.duration;
+            video.currentTime = videoFrame;
+        }
+    }, 50);
+});
