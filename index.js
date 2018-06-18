6c41b4a83e697f545402c5a638dbbfe22964b934
Brandon Tsang <brandononline2@gmail.com>
2018-06-18 09:58:38 -0400

    haha

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 let videoFrame = 0;
-let pixelsPerFrame = 10;
+let pixelsPerFrame = 6.5;
 let pageHeight, video, loop;
 
 $(function() {
     video = document.getElementById('video');
 
-    video.addEventListener('canplay', function() {
-        pageHeight = video.duration * 24 * pixelsPerFrame;
-        document.getElementById('set-height').style.height = pageHeight + window.innerHeight + 'px';
-
-        loop = true;
-    });
+    video.addEventListener('loadeddata', setHeight);
+    window.addEventListener('resize', setHeight);
 
     setInterval(function() {
         if (loop) {
             videoFrame = (window.pageYOffset / pageHeight) * video.duration;
             if (Math.abs(videoFrame - video.currentTime) > 0.01) {
                 video.currentTime = videoFrame;
-                console.log(videoFrame);
             }
         }
     }, 20);
 });
+
+function setHeight() {
+    pageHeight = video.duration * 24 * pixelsPerFrame;
+    document.getElementById('set-height').style.height = pageHeight + window.innerHeight + 'px';
+    console.log("no");
+
+    loop = true;
+}
